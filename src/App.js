3ed17c5d703ae2cb28ef86f0a2e727a735c47f02
TerenceG01 <terencegerber01@gmail.com>
2025-01-26 20:54:50 +0100

    Ajout du composant principal App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TravelApp from './components/TravelApp';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TravelApp />
+    </div>
+  );
+}
+
+export default App;
